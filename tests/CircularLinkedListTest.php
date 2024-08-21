2048138954369746efc9139cbca06dc07c7cd5ac
sneko <supernekocat31@gmail.com>
2024-08-21 20:02:46 +0900

    feat:add function(length, search, toArray)

--- tests/CircularLinkedListTest.php
+++ tests/CircularLinkedListTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . '/../Node.php';
 require_once __DIR__ . '/../CircularLinkedList.php';
 
 class CircularLinkedListTest extends TestCase
@@ -67,6 +68,75 @@ class CircularLinkedListTest extends TestCase
         $this->assertEquals(2, $this->list->delete());
         $this->expectOutputString('1');
         echo $this->list;
+    }
+
+    /**
+     * リストの長さを返す
+     */
+    public function testLength(): void
+    {
+        $this->list->append(1);
+        $this->list->append(2);
+        $this->list->append(3);
+        $this->assertEquals(3, $this->list->length());
+    }
+
+    /**
+     * 空のリストの長さを返す
+     */
+    public function testLengthEmptyList(): void
+    {
+        $this->assertEquals(0, $this->list->length());
+    }
+
+    /**
+     * リストを配列に変換
+     */
+    public function testToArray(): void
+    {
+        $this->list->append(1);
+        $this->list->append(2);
+        $this->list->append(3);
+        $this->assertEquals([1, 2, 3], $this->list->toArray());
+    }
+
+    /**
+     * 空のリストを空の配列に変換
+     */
+    public function testToArrayEmptyList(): void
+    {
+        $this->assertEquals([], $this->list->toArray());
+    }
+
+    /**
+     * 存在するデータを探索した場合、要素を返す
+     */
+    public function testSearch(): void
+    {
+        $data = 'dummy';
+        $node = new Node($data);
+        $node->next = $node;
+        $this->list->append($data);
+        $this->assertEquals($node, $this->list->search($data));
+    }
+
+    /**
+     * 存在しないデータを探索した場合、falseを返す
+     */
+    public function testSearchNotData(): void
+    {
+        $data = 'dummy1';
+        $this->list->append('dummy2');
+        $this->assertEquals(false, $this->list->search($data));
+    }
+
+    /**
+     * 空のリストを探索した場合、falseを返す
+     */
+    public function testSearchToEmptyList(): void
+    {
+        $data = 'dummy';
+        $this->assertEquals(false, $this->list->search($data));
     }
 
     /**
